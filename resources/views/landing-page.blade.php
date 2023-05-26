80ad1f8de89dfc13dffe521c597563fb8a6b6f55
mintup29 <vickodanendra@gmail.com>
2023-05-26 15:04:03 +0700

    update view landing page

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -38,7 +38,7 @@
             <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
                 <div class="d-flex justify-content-center">
                     <div class="text-center">
-                        <h1 class="mx-auto my-0 mb-2 text-uppercase" style="font-size: 55px">SISTEM KERINGANAN UANG KULIAH TUNGGAL (UKT) </h1>
+                        <h1 class="mx-auto my-0 mb-2 text-uppercase">SISTEM KERINGANAN UANG KULIAH TUNGGAL (UKT) </h1>
                         <h2 class="text-white-50 mx-auto mt-2 fs-6">FAKULTAS TEKNOLOGI INFORMASI DAN SAINS DATA</h2>
                         <h2 class="text-white-50 mx-auto mt-2 mb-5 fs-2">FATISDA</h2>
                         <a class="btn btn-primary" href="#about">Get Started</a>
