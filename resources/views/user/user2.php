67cf01c971da04693a9f228d4734f13c32fbeb5c
mintup29 <vickodanendra@gmail.com>
2023-05-27 17:49:53 +0700

    update user2

--- resources/views/user/user2.php
+++ resources/views/user/user2.php
@@ -1,47 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Sistem Keringanan UKT - FATISDA</title>
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="css/styles.css" rel="stylesheet" />
-    </head>
-    </body>
-        <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-            <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="#page-top"><img class="img-fluid" style="width: auto; height: 50px;" src="assets/img/LOGO 1.png" alt="..." /></a>
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#projects">Guide</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#signup">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <div class="row">
-            <div class="col-md-3 border-right">
-                <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"><span class="font-weight-bold">Cek</span><span class="text-black-50">cek@uns.student.uns.ac.id</span><span> </span></div>
-            </div>
-            <div class="col-md-5">
-                <div class="p-3 py-5">
-                    <div class="d-flex justify-content-between align-items-center mb-3">
-                        <!-- <h4 class="text-right">Profile Settings</h4> -->
-                    </div>
-                </div>
-            </div>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pengajuan Keringanan UKT</title>
+
+    <link href="/css/styles.css" rel="stylesheet" />
+    <link href="/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link href="/css/custom.css" rel="stylesheet" />
+    <!-- start navbar -->
+    <nav class="navbar fixed-top navbar-custom">
+        <img class="rounded ml-auto" alt="profile picture" src="https://pkptki.lppm.uns.ac.id/wp-content/uploads/sites/12/2022/04/Haryono-Setiadi-ST.-M.Eng_-234x300.jpg" />
+        <a>Haryono</a>
+    </nav>
+    <!-- end navbar -->
+</head>
+
+<body>
+    <div class="row h-100">
+    <div class="container col-lg-5 h-100" style="text-align: center;">
+        <img class="rounded-big ml-auto shadow" alt="profile picture" src="https://pkptki.lppm.uns.ac.id/wp-content/uploads/sites/12/2022/04/Haryono-Setiadi-ST.-M.Eng_-234x300.jpg" /><br>
+        <div class="profile-text shadow">Haryono</div>
+        <div class="profile-text shadow">Informatika</div>
+    </div>
+    <div class="container col-lg-7 shadow" >
+        <div >
+            <div>Cek</div>
         </div>
-    <body>
+    </div>
+    </div>
+</body>
+
+<script type="text/javascript" src="/js/bootstrap.js"></script>
+
 </html>
