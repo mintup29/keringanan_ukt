17b7e550f282bacc0b1a961d90cfab4fda18bf91
Yudistira Adi Pradana <yudistiraadipradana@student.uns.ac.id>
2023-05-27 18:04:55 +0700

    kuesioner_tahap_1

---
+++ resources/views/user/kuesioner.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pengajuan Keringanan UKT</title>
+    <link href="/css/styles.css" rel="stylesheet" />
+    <link href="/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link href="/css/custom.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <!-- start navbar -->
+    <nav class="navbar fixed-top navbar-custom">
+        <img class="rounded ml-auto" alt="profile picture" src="https://pkptki.lppm.uns.ac.id/wp-content/uploads/sites/12/2022/04/Haryono-Setiadi-ST.-M.Eng_-234x300.jpg" />
+        <a>Haryono</a>
+    </nav>
+    <!-- end navbar -->
+</head>
+<body>
+    <div class="row h-100">
+    <div class="container col-lg-5 h-100" style="text-align: center; width: 150px;">
+        <div class="profile-text shadow">Kuesioner Keringanan UKT</div>
+    </div>
+    
+</body>
+</html>
