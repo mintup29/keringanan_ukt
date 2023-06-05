58208eebcc18968b35a39195718da9dd73174977
HumanRight19 <Dutaandra19@gmail.com>
2023-06-06 06:31:42 +0700

    tambah isi skors

--- database/migrations/2023_06_05_082751_create_jawabans_table.php
+++ database/migrations/2023_06_05_082751_create_jawabans_table.php
@@ -15,8 +15,67 @@ return new class extends Migration
             $table->id();
             $table->foreignId('id_pertanyaan')->constrained('pertanyaans');
             $table->text('jawaban');
-            $table->timestamps();
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
+
+        $createMultipleJawabans = [
+            ['id_pertanyaan'=>1,'jawaban'=>'<3'],
+            ['id_pertanyaan'=>1,'jawaban'=>'3,0 - 5,0'],
+            ['id_pertanyaan'=>1,'jawaban'=>'5,1 - 7'],
+            ['id_pertanyaan'=>1,'jawaban'=>'>7'],
+            ['id_pertanyaan'=>2,'jawaban'=>'Lengkap'],
+            ['id_pertanyaan'=>2,'jawaban'=>'Piatu (Ibu Meninggal)'],
+            ['id_pertanyaan'=>2,'jawaban'=>'Yatim (Bapak Meninggal)'],
+            ['id_pertanyaan'=>2,'jawaban'=>'Yatim Piatu'],
+            ['id_pertanyaan'=>3,'jawaban'=>'PNS Gol III & IV / Pegawai BUMN'],
+            ['id_pertanyaan'=>3,'jawaban'=>'Karyawan Swasta / PNS Gol II dan I / Pensiunan PNS / BUMN'],
+            ['id_pertanyaan'=>3,'jawaban'=>'Petani / Pedagang / Pensiunan Swasta'],
+            ['id_pertanyaan'=>3,'jawaban'=>'Serabutan'],
+            ['id_pertanyaan'=>4,'jawaban'=>'Ya'],
+            ['id_pertanyaan'=>4,'jawaban'=>'Tidak'],
+            ['id_pertanyaan'=>5,'jawaban'=>'Kedua Orang Tua'],
+            ['id_pertanyaan'=>5,'jawaban'=>'Salah Satu'],
+            ['id_pertanyaan'=>6,'jawaban'=>'>8jt'],
+            ['id_pertanyaan'=>6,'jawaban'=>'>5jt'],
+            ['id_pertanyaan'=>6,'jawaban'=>'>2,5 - 5jt'],
+            ['id_pertanyaan'=>6,'jawaban'=>'<2,5jt'],
+            ['id_pertanyaan'=>7,'jawaban'=>'1'],
+            ['id_pertanyaan'=>7,'jawaban'=>'2'],
+            ['id_pertanyaan'=>7,'jawaban'=>'3'],
+            ['id_pertanyaan'=>7,'jawaban'=>'>3'],
+            ['id_pertanyaan'=>8,'jawaban'=>'>3'],
+            ['id_pertanyaan'=>8,'jawaban'=>'3'],
+            ['id_pertanyaan'=>8,'jawaban'=>'2'],
+            ['id_pertanyaan'=>8,'jawaban'=>'1'],
+            ['id_pertanyaan'=>9,'jawaban'=>'<3'],
+            ['id_pertanyaan'=>9,'jawaban'=>'3'],
+            ['id_pertanyaan'=>9,'jawaban'=>'4'],
+            ['id_pertanyaan'=>9,'jawaban'=>'>5'],
+            ['id_pertanyaan'=>10,'jawaban'=>'Menikah'],
+            ['id_pertanyaan'=>10,'jawaban'=>'SD / SMP / SMA'],
+            ['id_pertanyaan'=>10,'jawaban'=>'Tidak / Belum Menikah'],
+            ['id_pertanyaan'=>10,'jawaban'=>'Kuliah'],
+            ['id_pertanyaan'=>11,'jawaban'=>'Milik Pribadi / Rumah Dinas'],
+            ['id_pertanyaan'=>11,'jawaban'=>'Menumpang'],
+            ['id_pertanyaan'=>11,'jawaban'=>'Sewa / Kontrak'],
+            ['id_pertanyaan'=>12,'jawaban'=>'Tembok Permanen, Keramik Luas Lebih 80 meter'],
+            ['id_pertanyaan'=>12,'jawaban'=>'Semi Permanen, Keramik'],
+            ['id_pertanyaan'=>12,'jawaban'=>'Semi Permanen, Tidak Keramik'],
+            ['id_pertanyaan'=>12,'jawaban'=>'Tidak Permanen'],
+            ['id_pertanyaan'=>13,'jawaban'=>'>500.000'],
+            ['id_pertanyaan'=>13,'jawaban'=>'>250.000 - 500.000'],
+            ['id_pertanyaan'=>13,'jawaban'=>'>150.000 - 250.000'],
+            ['id_pertanyaan'=>13,'jawaban'=>'<150.000'],
+            ['id_pertanyaan'=>14,'jawaban'=>'Roda 4 (1 buah) dan atau 4 meter'],
+            ['id_pertanyaan'=>14,'jawaban'=>'3 meter'],
+            ['id_pertanyaan'=>14,'jawaban'=>'2 meter'],
+            ['id_pertanyaan'=>14,'jawaban'=>'1 meter']
+        ];
+        
+        //User::insert($createMultipleUsers); // Eloquent
+        \DB::table('jawabans')->insert($createMultipleJawabans); // Query Builder
+
     }
 
     /**
