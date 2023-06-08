c4940495a9e5fe260551e4abeb0a5bf8ef014f1c
HumanRight19 <Dutaandra19@gmail.com>
2023-06-08 22:16:56 +0700

    tambahan

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\PengajuanMahasiswaController;
 use App\Http\Controllers\PertanyaanController;
 use App\Http\Controllers\KuesionerController;
-=======
 use App\Http\Controllers\MhsController;
 use App\Http\Controllers\AuthController;
->>>>>>> f6650a7e1465908dd854f5ea958c198a85affbef
 
 /*
 |--------------------------------------------------------------------------
@@ -25,22 +22,18 @@ Route::group(['middleware' => 'guest'], function(){
     Route::get('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
 });
-<<<<<<< HEAD
 Route::get('/login', function(){
     return view('auth/sso');
-=======
 Route::get('/logout', [Authcontroller::class, 'logout'])->name('logout');
 
 Route::middleware(['auth', 'isAdmin'])->group(function(){
     Route::get('/admin', function(){return view('admin');})->name('admin');
->>>>>>> f6650a7e1465908dd854f5ea958c198a85affbef
 });
 
 Route::middleware(['auth', 'isUser'])->group(function(){
     Route::get('/pengajuan', [MhsController::class, 'mhsDashboard'])->name('pengajuan');
     Route::get('/kuesioner', function(){return view('user.kuesioner');})->name('kuesioner');
 });
-<<<<<<< HEAD
 Route::get('/pengajuan_2', function(){
     return view('user/user2');
 });
@@ -67,6 +60,4 @@ Route::get('/dashboard-admin', [PengajuanMahasiswaController::class, 'index'])->
 Route::put('/dashboard-admin/{item}/update-action', [PengajuanMahasiswaController::class, 'updateAction'])->name('pengajuan.update-action');
 
 Route::get('/getPengajuan', [\App\Http\Controllers\PengajuanMahasiswaController::class, 'index'])->name('getPengajuan');
-=======
 Route::get('/', function () {return view('landing-page');})->name('home');
->>>>>>> f6650a7e1465908dd854f5ea958c198a85affbef
