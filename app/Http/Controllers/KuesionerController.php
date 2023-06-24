2117670401d5a3f37ecccfd763351b15f95356e0
Yudistira Adi Pradana <yudistiraadipradana@student.uns.ac.id>
2023-06-24 20:25:13 +0700

    kuesioner

--- app/Http/Controllers/KuesionerController.php
+++ app/Http/Controllers/KuesionerController.php
@@ -2,26 +2,18 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
-use App\Models\JawabanMahasiswa;
-=======
 use App\Models\Jawaban;
 use App\Models\JawabanMahasiswa;
 use App\Models\Mahasiswa;
 use App\Models\PengajuanMahasiswa;
->>>>>>> e53249a1fc5b8fb8b76eb1d894b41761ad38c379
 use App\Models\Pertanyaan;
 use App\Models\Skor;
 use App\Models\User;
-use App\Models\Cek;
 use View;
 use Illuminate\Http\Request;
 use App\Post;
-<<<<<<< HEAD
-=======
 use DB;
 use Auth;
->>>>>>> e53249a1fc5b8fb8b76eb1d894b41761ad38c379
 
 class KuesionerController extends Controller
 {
@@ -61,21 +53,6 @@ class KuesionerController extends Controller
         // return View::make('user.kuesioner');
         return View::make('user.kuesioner')->with('pertanyaan', $pertanyaans)->with('jawabanskors', $jawabanskors)->with('user',$user); //return the view with posts
     }
-<<<<<<< HEAD
-    public function store(Request $request){
-        $jawaban = new JawabanMahasiswa();
-        $input = $request->input('pertanyaan{{ $item->id }}');
-        $jawaban->id_pertanyaan = $input;
-        $jawaban->id_jawaban = $input;
-        $jawaban->id_jawaban = $input;
-        $jawaban->id_mahasiswa = $input;
-        $jawaban->id_pengajuan_mahasiswa = $input;
-        $jawaban->id_skor = $input;
-        $jawaban->save();
-        return redirect()->back()->with('success', 'Pertanyaan berhasil ditambahkan');
-    }
-}
-=======
     
     public function store(Request $request, $id){
         
@@ -86,63 +63,6 @@ class KuesionerController extends Controller
         //     'id_skor'=>'required'
         // ]);
 
-        // dd($request);
-
-        // Assuming you have established a database connection and retrieved the request object
-
-        $userId = $request->input('user_id');
-        $idPertanyaan = $request->input('id_pertanyaan');
-        $idJawaban = $request->input('id_jawaban');
-        $idSkor = $request->input('id_skor');
-
-        PengajuanMahasiswa::create([
-            'id_mahasiswa' => $userId,
-            'status' => 'Need Action',
-            'skor_total' => '0',
-            'potongan' => '0',
-            'semester' => '1',
-            'tahun' => '2021',
-        ]);
-
-        $idPengajuan = PengajuanMahasiswa::latest('id')->first();
-        $idSubmission = $idPengajuan->id;
-
-        $scoreTotal = 0;
-
-        foreach ($idPertanyaan as $key => $questionId) {
-            $question = $questionId;
-            $answer = $idJawaban[$key];
-            $score = $idSkor[$key];
-
-            $scoreTotal += $score;
-            			
-            JawabanMahasiswa::create([
-                'id_pengajuan_mahasiswa' => $idPengajuan->id, 
-                'id_mahasiswa' => $userId,
-                'id_pertanyaan' => $question,
-                'id_jawaban' => $answer,
-                'id_skor' => $score,
-            ]);
-        }
-
-        PengajuanMahasiswa::where('id', $idSubmission)->update(['skor_total' => $scoreTotal]);
-
-        $percentage = '';
-        if ($scoreTotal >= 40 && $scoreTotal <= 54) {
-            $percentage = '30%';
-        } elseif ($scoreTotal >= 31 && $scoreTotal < 40) {
-            $percentage = '20%';
-        } elseif ($scoreTotal >= 26 && $scoreTotal < 31) {
-            $percentage = '10%';
-        }
-
-        PengajuanMahasiswa::where('id', $idSubmission)->update(['potongan' => $percentage]);
-
-
-
-        // return redirect()->route('user.profil')->with('success','Kuesioner berhasil diisi');
-
-
         // $pertanyaan = Pertanyaan::count();
 
         // for($i=1; $i <= $pertanyaan; $i++) {
@@ -152,8 +72,8 @@ class KuesionerController extends Controller
         //     $id_mahasiswa = $request->input('user_id');
 
         //     $jawaban = new JawabanMahasiswa;
-        //     $user = User::where('id', $id)->get('name');
-        //     $mahasiswa = Mahasiswa::where('nama', $user)->get('id');
+        //     // $user = User::where('id', $id)->get('name');
+        //     // $mahasiswa = Mahasiswa::where('nama', $user)->get('id');
         //     // $pengajuan_mahasiswa = PengajuanMahasiswa::where('id_mahasiswa', $mahasiswa)->get();
             
         //     $jawaban->id_mahasiswa = $id_mahasiswa;
@@ -161,13 +81,40 @@ class KuesionerController extends Controller
         //     $jawaban->id_pertanyaan = $id_pertanyaan;
         //     $jawaban->id_jawaban = $id_jawaban;
         //     $jawaban->id_skor = $id_skor;
-    
         //     $jawaban->save();
         // }
+        $userId = $request->input('user_id');
+        $idPertanyaan = $request->input('id_pertanyaan');
+        $idJawaban = $request->input('id_jawaban');
+        $idSkor = $request->input('id_skor');
 
-        // dd($request);
-        // return redirect()->back();
-        // return View::make('user.profil');
+        PengajuanMahasiswa::create([
+            'id_mahasiswa' => $userId,
+            'status' => 'Need Action',
+            'skor_total' => '0',
+            'semester' => 2,
+            'tahun' => 2023,
+        ]);
+
+        $idpengajuan = PengajuanMahasiswa::latest('id')->first();
+        $idpengajuanlast = $idpengajuan -> id;
+        $skor_total = 0;
+        foreach ($idPertanyaan as $key => $questionId) {
+            $questionIdea = $questionId;
+            $answerIdea = $idJawaban[$key];
+            $scoreIdea = $idSkor[$key];
+            $skor_total += $scoreIdea;
+            			
+            JawabanMahasiswa::create([
+                'id' => $userId,
+                'id_mahasiswa' => $userId,
+                'id_pengajuan_mahasiswa' => $idpengajuanlast,
+                'id_pertanyaan' => $questionIdea,
+                'id_jawaban' => $answerIdea,
+                'id_skor' => $scoreIdea,
+            ]);
+        }
+
+        return View::make('user.profil');
     }
 }
->>>>>>> e53249a1fc5b8fb8b76eb1d894b41761ad38c379
